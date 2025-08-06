c6cb974b40391fa73e75cba860112d41de841129
Sergi Bray <sergibpublic@gmail.com>
2025-08-06 22:08:33 +0100

    a

--- defy-website/src/App.tsx
+++ defy-website/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Logo } from "./assets/jsx_of_svg/logo";
-import { TabSVG } from "./assets/jsx_of_svg/tab";
+// import { TabSVG } from "./assets/jsx_of_svg/tab";
 import Findings from "./components/findings";
 import Donate from "./components/donate";
 import Mission from "./components/mission";
@@ -23,10 +23,10 @@ function App() {
             height: "10vh",
             display: "flex",
             flexDirection: "row",
-            backgroundColor: "blue",
+            // backgroundColor: "blue",
             placeContent: "center",
             placeSelf: "center",
-            // placeItems: "center",
+            placeItems: "center",
           }}
         >
           <div
@@ -34,171 +34,31 @@ function App() {
             style={{
               color: "black",
               margin: "0",
-              height: "15vh",
-              width: "15vw",
+              height: "90%",
+              width: "auto",
+              paddingTop: "5%",
+              paddingBottom: "5%",
               placeContent: "center",
               placeSelf: "center",
               userSelect: "none",
             }}
+            onClick={() => setActiveTab(DonateTab.id)}
           >
             {/* {isBigScreen && <Logo />} */}
             <Logo />
           </div>
-          topbar
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              paddingTop: "2em",
-              paddingBottom: "2em",
-              fontFamily: "Cafe",
-              width: "12vw",
-            }}
-          >
-            {tabs.map(function (tab) {
-              return (
-                <div
-                  style={{
-                    width: "20vw",
-                    paddingTop: "10px",
-                    paddingBottom: "10px",
-                    height: "7vh",
-                    position: "relative",
-                  }}
-                  onClick={() => setActiveTab(tab.id)}
-                >
-                  {activeTab == tab.id ? (
-                    <div>
-                      <div
-                        style={{
-                          zIndex: 0,
-                          position: "absolute",
-                          height: "7vh",
-                          width: "20vw",
-                          top: 0,
-                        }}
-                      >
-                        <TabSVG />
-                      </div>
-                      <div
-                        style={{
-                          zIndex: 1,
-                          height: "7vh",
-                          width: "10vw",
-                          position: "absolute",
-                          top: 0,
-                          left: "75px",
-                          alignContent: "center",
-                          textAlign: "left",
-                        }}
-                      >
-                        <h2
-                          style={{
-                            fontWeight: "900",
-                            marginRight: "-30px",
-                          }}
-                        >
-                          {tab.name}
-                        </h2>
-                      </div>
-                    </div>
-                  ) : (
-                    <div>
-                      <div
-                        style={{
-                          zIndex: 0,
-                          position: "absolute",
-                          height: "7vh",
-                          width: "20vw",
-                          right: "-150px",
-                          top: 0,
-                        }}
-                      >
-                        {/* <TabSVG /> */}
-                      </div>
-                      <div
-                        style={{
-                          zIndex: 1,
-                          height: "7vh",
-                          width: "10vw",
-                          alignContent: "center",
-                          position: "absolute",
-                          top: 0,
-                        }}
-                      >
-                        <h2
-                          style={{
-                            position: "absolute",
-                            alignSelf: "center",
-                            right: "-110px",
-                            textAlign: "right",
-                            color: "rgb(35,31,32)",
-                          }}
-                        >
-                          {tab.name}
-                        </h2>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              );
-            })}
+          <div style={{ marginLeft: "2em" }}>
+            <button>mission</button>
+            <button>research</button>
+            <button>who</button>
+            <button>goals</button>
           </div>
-          {activeTab == DonateTab.id ? (
-            <div
-              style={{
-                width: "80%",
-                backgroundColor: "rgb(50,220,94)",
-                right: "18px",
-                height: "15vh",
-                placeSelf: "center",
-                placeContent: "center",
-                position: "relative",
-                borderRadius: "5px",
-                border: "9px solid rgb(35,31,32)",
-              }}
-              onClick={() => setActiveTab(DonateTab.id)}
-            >
-              <h2
-                style={{
-                  fontFamily: "AnotherTypewriter",
-                  fontWeight: "900",
-                  color: "black",
-                  textShadow:
-                    "0px 0px 1px white, 0 0 25px white, 0 0 5px white",
-                }}
-              >
-                {DonateTab.name}
-              </h2>
-            </div>
-          ) : (
-            <div
-              style={{
-                width: "80%",
-                backgroundColor: "black",
-                right: "18px",
-                height: "15vh",
-                placeSelf: "center",
-                placeContent: "center",
-                position: "relative",
-                borderRadius: "5px",
-                animation: "tremble 4000ms",
-                animationIterationCount: "infinite",
-                border: "9px solid rgb(35,31,32)",
-              }}
-              onClick={() => setActiveTab(DonateTab.id)}
-            >
-              <h2
-                style={{
-                  fontFamily: "AnotherTypewriter",
-                  fontWeight: "900",
-                  color: "rgb(50,220,94)",
-                }}
-              >
-                {DonateTab.name}
-              </h2>
-            </div>
-          )}
+          <div
+            style={{ marginLeft: "2em" }}
+            onClick={() => setActiveTab(DonateTab.id)}
+          >
+            <button>donate!</button>
+          </div>
         </div>
       </>
     );
@@ -213,9 +73,8 @@ function App() {
             height: "70vh",
             display: "flex",
             flexDirection: "column",
-            backgroundColor: "blue",
+            // backgroundColor: "blue",
             placeContent: "center",
-            // placeItems: "center",
             placeSelf: "center",
           }}
         >
@@ -230,9 +89,9 @@ function App() {
               placeSelf: "center",
               userSelect: "none",
             }}
+            onClick={() => setActiveTab(DonateTab.id)}
           >
             {isBigScreen && <Logo />}
-            {/* <Logo /> */}
           </div>
           <div
             style={{
@@ -241,14 +100,14 @@ function App() {
               paddingTop: "2em",
               paddingBottom: "2em",
               fontFamily: "Cafe",
-              width: "12vw",
+              width: "100%",
             }}
           >
             {tabs.map(function (tab) {
               return (
                 <div
                   style={{
-                    width: "20vw",
+                    width: "100%",
                     paddingTop: "10px",
                     paddingBottom: "10px",
                     height: "7vh",
@@ -257,76 +116,37 @@ function App() {
                   onClick={() => setActiveTab(tab.id)}
                 >
                   {activeTab == tab.id ? (
-                    <div>
-                      <div
-                        style={{
-                          zIndex: 0,
-                          position: "absolute",
-                          height: "7vh",
-                          width: "20vw",
-                          top: 0,
-                        }}
-                      >
-                        <TabSVG />
-                      </div>
-                      <div
-                        style={{
-                          zIndex: 1,
-                          height: "7vh",
-                          width: "10vw",
-                          position: "absolute",
-                          top: 0,
-                          left: "75px",
-                          alignContent: "center",
-                          textAlign: "left",
-                        }}
-                      >
-                        <h2
-                          style={{
-                            fontWeight: "900",
-                            marginRight: "-30px",
-                          }}
-                        >
-                          {tab.name}
-                        </h2>
-                      </div>
+                    <div
+                      style={{
+                        zIndex: 1,
+                        height: "7vh",
+                        width: "100%",
+                        position: "absolute",
+                        backgroundColor: "black",
+                        alignContent: "center",
+                        textAlign: "center",
+                      }}
+                    >
+                      <h2>{tab.name}</h2>
                     </div>
                   ) : (
-                    <div>
-                      <div
+                    <div
+                      style={{
+                        zIndex: 1,
+                        height: "7vh",
+                        width: "100%",
+                        alignContent: "center",
+                      }}
+                    >
+                      <h2
                         style={{
-                          zIndex: 0,
-                          position: "absolute",
-                          height: "7vh",
-                          width: "20vw",
-                          right: "-150px",
-                          top: 0,
+                          textAlign: "right",
+                          paddingRight: "1em",
+                          color: "rgb(35,31,32)",
                         }}
                       >
-                        {/* <TabSVG /> */}
-                      </div>
-                      <div
-                        style={{
-                          zIndex: 1,
-                          height: "7vh",
-                          width: "10vw",
-                          alignContent: "center",
-                          position: "absolute",
-                          top: 0,
-                        }}
-                      >
-                        <h2
-                          style={{
-                            position: "absolute",
-                            alignSelf: "center",
-                            right: "-110px",
-                            textAlign: "right",
-                            color: "rgb(35,31,32)",
-                          }}
-                        >
-                          {tab.name}
-                        </h2>
-                      </div>
+                        {tab.name}
+                      </h2>
                     </div>
                   )}
                 </div>
@@ -349,26 +169,39 @@ function App() {
                   backgroundColor: "rgb(50,220,94)",
                   placeSelf: "center",
                   placeContent: "center",
-                  paddingLeft:'2em',
-                  paddingRight:'2em',
+                  paddingLeft: "2em",
+                  paddingRight: "2em",
                   borderRadius: "5px",
                   border: "9px solid rgb(35,31,32)",
                 }}
                 onClick={() => setActiveTab(DonateTab.id)}
               >
-                <h2
-                  style={{
-                    fontFamily: "AnotherTypewriter",
-                    fontWeight: "900",
-                    color: "black",
-                    textShadow:
-                      "0px 0px 1px white, 0 0 25px white, 0 0 5px white",
-                  }}
-                >
-                  {/* {DonateTab.name} */}
-                  {/* Keep Our \nLights On! */}
-                  Keep Our <br/>Lights On!
-                </h2>
+                {isHugeScreen ? (
+                  <h2
+                    style={{
+                      fontFamily: "AnotherTypewriter",
+                      fontWeight: "900",
+                      color: "black",
+                      textShadow:
+                        "0px 0px 1px white, 0 0 25px white, 0 0 5px white",
+                    }}
+                  >
+                    Keep Our <br />
+                    Lights On!
+                  </h2>
+                ) : (
+                  <h2
+                    style={{
+                      fontFamily: "AnotherTypewriter",
+                      fontWeight: "900",
+                      color: "black",
+                      textShadow:
+                        "0px 0px 1px white, 0 0 25px white, 0 0 5px white",
+                    }}
+                  >
+                    Donate
+                  </h2>
+                )}
               </div>
             ) : (
               <div
@@ -379,24 +212,36 @@ function App() {
                   placeContent: "center",
                   position: "relative",
                   borderRadius: "5px",
-                  paddingLeft:'2em',
-                  paddingRight:'2em',
+                  paddingLeft: "2em",
+                  paddingRight: "2em",
                   animation: "tremble 4000ms",
                   animationIterationCount: "infinite",
                   border: "9px solid rgb(35,31,32)",
                 }}
                 onClick={() => setActiveTab(DonateTab.id)}
               >
-                <h2
-                  style={{
-                    fontFamily: "AnotherTypewriter",
-                    fontWeight: "900",
-                    color: "rgb(50,220,94)",
-                  }}
-                >
-                  {/* {DonateTab.name} */}
-                  Keep Our <br/>Lights On!
-                </h2>
+                {isHugeScreen ? (
+                  <h2
+                    style={{
+                      fontFamily: "AnotherTypewriter",
+                      fontWeight: "900",
+                      color: "rgb(50,220,94)",
+                    }}
+                  >
+                    Keep Our <br />
+                    Lights On!
+                  </h2>
+                ) : (
+                  <h2
+                    style={{
+                      fontFamily: "AnotherTypewriter",
+                      fontWeight: "900",
+                      color: "rgb(50,220,94)",
+                    }}
+                  >
+                    Donate
+                  </h2>
+                )}
               </div>
             )}
           </div>
@@ -413,7 +258,6 @@ function App() {
             fontFamily: "Cafe",
             fontSize: "x-large",
             backgroundColor: "white",
-            border: "4px solid rgb(35,31,32)",
             zIndex: 2,
           }}
         >
@@ -441,6 +285,9 @@ function App() {
   const isBigScreen = useMediaQuery({
     query: "(min-width: 1224px)",
   });
+  const isHugeScreen = useMediaQuery({
+    query: "(min-width: 1824px)",
+  });
   class Tab {
     id: string;
     name: string;
@@ -449,11 +296,11 @@ function App() {
       this.name = name;
     }
   }
-  const IntroTab = new Tab("intro", "Mission Statement");
-  const FiguresTab = new Tab("figures", "Research Findings");
-  const WhoTab = new Tab("who", "Who Are We?");
-  const WhatTab = new Tab("what", "Our Next Goals");
-  const DonateTab = new Tab("donate", "Keep Our Lights On!");
+  const IntroTab = new Tab("intro", "Mission");
+  const FiguresTab = new Tab("figures", "Research");
+  const WhoTab = new Tab("who", "Who");
+  const WhatTab = new Tab("what", "Goals");
+  const DonateTab = new Tab("donate", "Donate");
   const tabs = [IntroTab, FiguresTab, WhoTab, WhatTab];
   return (
     <>
@@ -487,7 +334,6 @@ function App() {
             id="centre_panel"
             style={{
               display: "flex",
-              backgroundColor: "white",
               placeItems: "center",
               placeContent: "center",
               flexDirection: "column",
