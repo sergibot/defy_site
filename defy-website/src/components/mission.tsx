faddd7fe88d88b8571c95e04e8386235818b2405
Sergi Bray <sergibpublic@gmail.com>
2025-08-08 10:55:08 +0100

    a

--- defy-website/src/components/mission.tsx
+++ defy-website/src/components/mission.tsx
@@ -20,10 +20,137 @@ import DfiaSites from "../assets/jsx_of_svg/dfia_sites";
 function Mission() {
   return (
     <div>
-      In mid-2023, an academic survey of over 16,000 participants across 10 countries found that 2.2% of participants were victim-survivors of deepfake image-based sexual abuse. 1.8% of participants admitted to perpetrating this sort of abuse. However, these numbers were taken before the AI bubble had begun to hit. The numbers today are not available - yet another product of the AI overwhelm, but a critical one for understanding the amount of harm that this type of sexual abuse is wreaking globally.
-      <button>link to study</button>
-
-      <button>click here for graph</button>
+      <h2>DFIAM is a serious yet unmonitored harm.</h2>
+      <div>
+        In mid-2023, an academic survey of over 16,000 participants across 10
+        countries found that 2.2% of participants were victim-survivors of
+        deepfake image-based sexual abuse. 1.8% of participants admitted to
+        perpetrating this sort of abuse. However, these numbers were taken
+        before the AI bubble had begun to hit. The numbers today are not
+        available - yet another product of the AI overwhelm, but a critical one
+        for understanding the amount of harm that this type of sexual abuse is
+        wreaking globally.
+      </div>
+      <div>
+        <button>link to study</button>
+      </div>
+      <div>
+        <button>click here for graph</button>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th>Country</th>
+            <th colSpan={2}>Victims</th>
+            <th colSpan={2}>Perpetrators</th>
+            <th>Population</th>
+          </tr>
+          <tr>
+            <th>(age 18+)</th>
+            <th>%</th>
+            <th>N</th>
+            <th>%</th>
+            <th>N</th>
+            <th></th>
+          </tr>
+          <tr>
+            <td>Australia</td>
+            <td>3.7</td>
+            <td>790,213</td>
+            <td>2.4</td>
+            <td>512,571</td>
+            <td>21,357,108</td>
+          </tr>
+          <tr>
+            <td>South Korea</td>
+            <td>3.1</td>
+            <td>1,909,439</td>
+            <td>1.2</td>
+            <td>739,138</td>
+            <td>61,594,800</td>
+          </tr>
+          <tr>
+            <td>Mexico</td>
+            <td>2.9</td>
+            <td>2,545,216</td>
+            <td>1.2</td>
+            <td>1,053,193</td>
+            <td>87,766,066</td>
+          </tr>
+          <tr>
+            <td>USA</td>
+            <td>2.3</td>
+            <td>6,027,910</td>
+            <td>2.6</td>
+            <td>6,814,159</td>
+            <td>262,083,034</td>
+          </tr>
+          <tr>
+            <td>France</td>
+            <td>1.6</td>
+            <td>829,985</td>
+            <td>1.1</td>
+            <td>570,615</td>
+            <td>51,874,048</td>
+          </tr>
+          <tr>
+            <td>Spain</td>
+            <td>1.4</td>
+            <td>560,100</td>
+            <td>0.8</td>
+            <td>320,057</td>
+            <td>40,007,177</td>
+          </tr>
+          <tr>
+            <td>Netherlands</td>
+            <td>1.0</td>
+            <td>141,642</td>
+            <td>0.4</td>
+            <td>56,657</td>
+            <td>14,164,193</td>
+          </tr>
+          <tr>
+            <td>Denmark</td>
+            <td>1.0</td>
+            <td>48,518</td>
+            <td>0.7</td>
+            <td>33,963</td>
+            <td>4,851,818</td>
+          </tr>
+          <tr>
+            <td>Poland</td>
+            <td>0.9</td>
+            <td>277,420</td>
+            <td>0.5</td>
+            <td>154,122</td>
+            <td>30,824,437</td>
+          </tr>
+          <tr>
+            <td>Belgium</td>
+            <td>0.7</td>
+            <td>64,464</td>
+            <td>0.3</td>
+            <td>27,627</td>
+            <td>9,209,116</td>
+          </tr>
+          <tr>
+            <td>Totalled</td>
+            <td>2.2</td>
+            <td>13,194,907</td>
+            <td>1.8</td>
+            <td>10,282,101</td>
+            <td>583,731,797</td>
+          </tr>
+          <tr>
+            <td>Globe (age 15+)</td>
+            <td>2.2</td>
+            <td>136,310,574</td>
+            <td>1.8</td>
+            <td>111,526,833</td>
+            <td>6,195,935,158</td>
+          </tr>
+        </table>
+      </div>
       {/* <GgTrends/> */}
       {/* <div
         id="site_graph_container"
